8bec1a28dc131875450a82e83057dc4e63cfb3ba
Luiz <luiz646567@gmail.com>
2025-02-09 11:08:33 -0300

    atualização de estilização das páginas de cadastro e login.

--- venda/models.py
+++ venda/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class CustomUser(AbstractUser):
-    is_moderator = models.BooleanField(default=False)  # Campo extra para o moderador
+    is_moderator = models.BooleanField(default=False) 
 
     def __str__(self):
         return self.username
--- venda/views.py
+++ venda/views.py
@@ -1,30 +1,29 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
-from venda.models import CustomUser  # Importando CustomUser
+from venda.models import CustomUser  
 from venda.forms import CadastroForm, LoginForm
 
-# Função para cadastro de novos usuários
+
 def cadastro(request):
     if request.method == 'POST':
         form = CadastroForm(request.POST)
         if form.is_valid():
-            form.save()  # Cria o usuário no banco de dados
-            return redirect('login')  # Redireciona para o login após o cadastro
+            form.save()  
+            return redirect('login')
     else:
         form = CadastroForm()
     return render(request, 'cadastro.html', {'form': form})
 
-# Função de login
 def login_view(request):
     if request.method == 'POST':
         form = LoginForm(request, data=request.POST)
         if form.is_valid():
             username = form.cleaned_data['username']
             password = form.cleaned_data['password']
-            user = authenticate(username=username, password=password)  # Verifica se o usuário é válido
+            user = authenticate(username=username, password=password)  
             if user is not None:
-                login(request, user)  # Realiza o login
-                return redirect('dashboard')  # Redireciona para a página do dashboard
+                login(request, user)  
+                return redirect('dashboard')  
     else:
         form = LoginForm()
     return render(request, 'login.html', {'form': form})
